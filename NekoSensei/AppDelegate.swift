980f03e0f354389f8eb086907fed1bcec000f6e4
matthew carpenter <mattcarpenter@gmail.com>
2016-06-18 12:31:51 -0700

    Initial Commit

---
+++ NekoSensei/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NekoSensei
+//
+//  Created by matthew carpenter on 6/18/16.
+//  Copyright © 2016 matthew carpenter. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
